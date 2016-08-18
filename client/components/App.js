3593c0756a1adab63cf22baf42ff5242d1a16d32
ANGEL <angelcelis.acommerce@gmail.com>
2016-08-19 06:10:36 +0800

    edit and save functionality

--- client/components/App.js
+++ client/components/App.js
@@ -9,10 +9,16 @@ export default class App extends Component{
     super(props);
     this.state = {
       item: '',
-      todoList: ['item1', 'item2']
+      todoList: ['item1', 'item2'],
+      editingMode: false,
+      addButton: true
     }
     this.onTextChange = this.onTextChange.bind(this);
     this.addButton = this.addButton.bind(this);
+    // this.onEdit = this.onEdit.bind(this);
+    // this.onSave = this.onSave.bind(this);
+    // this.editMode = this.editMode.bind(this);
+    // this.saveMode = this.saveMode.bind(this);
   }
 
   onTextChange(e){
@@ -32,13 +38,54 @@ export default class App extends Component{
     // console.log('Clicked!');
     console.log(this.state.todoList);
   }
+  // onEdit(){
+  //   this.setState({
+  //     editingMode: true
+  //   });
+  // }
+  // onSave(){
+  //   let val = this.refs.newVal.value;
+  //   console.log("new value" + val);
+  //   this.setState({
+  //     editingMode: false
+  //   });
+  // }
+  // editMode(){
+  //   return(
+  //     <ul className="list-group">
+  //       {this.state.todoList.map((item, index) => (
+  //         <li className="list-group-item" key={index}>
+  //           <input type="checkbox"/> <span style={{marginRight:"80%"}}>{item}</span>
+  //           <button type="button" onClick={this.onEdit}>Edit</button>
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   );
+  // }
+  // saveMode(){
+  //   return(
+  //     <ul className="list-group">
+  //       {this.state.todoList.map((item, index) => (
+  //         <li className="list-group-item" key={index}>
+  //           <input type="text" value={item} ref="newVal" style={{marginRight:"60%"}}/>
+  //           <button type="button" onClick={this.onSave}>Save</button>
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   )
+  // }
+
   render(){
     // console.log(this.state.todoList);
     return(
       <div className="row" style={{marginTop: '50px'}}>
         <TodoAddBox onTextChange={this.onTextChange} addButton={this.addButton} item={this.state.item}/>
         <div className="col-md-6 col-md-offset-3 jumbotron  listContainer">
-          <TodoList todoList={this.state.todoList}/>
+          {this.state.todoList.map((item, index) => (
+            <TodoList key={index}>
+              {item}
+            </TodoList>
+          ))}
           <TodoButtons />
         </div>
       </div>
--- client/components/TodoList.js
+++ client/components/TodoList.js
@@ -2,13 +2,52 @@ import React, { Component } from 'react';
 import TodoButtons from './TodoButtons';
 
 export default class TodoList extends Component{
-  render(){
+  constructor(props){
+    super(props);
+    this.state = {
+      editingMode: false
+    }
+    this.onEdit = this.onEdit.bind(this);
+    this.onSave = this.onSave.bind(this);
+    this.editMode = this.editMode.bind(this);
+    this.saveMode = this.saveMode.bind(this);
+  }
+  onEdit(){
+    this.setState({
+      editingMode: true
+    });
+  }
+  onSave(){
+    this.setState({
+      editingMode: false
+    });
+  }
+  editMode(){
     return(
       <ul className="list-group">
-      {this.props.todoList.map((item, index) => (
-        <li className="list-group-item" key={index}><input type="checkbox" /> {item}</li>
-        ))}
+          <li className="list-group-item">
+            {this.props.children}
+            <button type="button" onClick={this.onEdit}>Edit</button>
+          </li>
+      </ul>
+    );
+  }
+  saveMode(){
+    return(
+      <ul className="list-group">
+          <li className="list-group-item">
+            <input type="text" defaultValue={this.props.children} style={{marginRight:"50%"}}/>
+            <button type="button" onClick={this.onSave}>Save</button>
+          </li>
       </ul>
     )
+  }
+  render(){
+    if(this.state.editingMode){
+      return this.saveMode();
+    }
+    else {
+      return this.editMode();
+    }
   }
 }
